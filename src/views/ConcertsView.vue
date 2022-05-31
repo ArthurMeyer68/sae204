132a16209cffe516412c8c614c7a559d0c86331c
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-05-31 13:33:50 +0200

    début page concerts

--- src/views/ConcertsView.vue
+++ src/views/ConcertsView.vue
@@ -1,12 +1,12 @@
-<template></template>
+<template>
+  <div class="bg-black">
+    <p class="text-center font-bold text-white sm:text-lg md:text-xl xl:text-2xl">LES CONCERTS</p>
+    <img src="../assets/img/lignes_page2.svg" class="ml-auto mr-auto mb-7 mt-3 w-3/12" />
+  </div>
+</template>
 
 <script>
-export default {
-  name: "MangasView",
-  data() {
-    return {};
-  },
-};
+export default {};
 </script>
 
 <style scoped>
