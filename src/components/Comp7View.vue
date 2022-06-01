47ee2267fbf44b4650e52e8652c5abab66d22912
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-01 12:54:07 +0200

    dark mode Composant7

--- src/components/Comp7View.vue
+++ src/components/Comp7View.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="pl-20">
-    <p class="text-white md:text-xl xl:text-2xl">SAMEDI</p>
+    <p class="text-white dark:text-black md:text-xl xl:text-2xl">SAMEDI</p>
     <div class="flex">
       <p class="text-red-600 md:text-3xl xl:text-5xl">11</p>
-      <p class="pl-3 text-white md:text-3xl xl:text-5xl">juin</p>
+      <p class="pl-3 text-white dark:text-black md:text-3xl xl:text-5xl">juin</p>
     </div>
 
-    <p class="pt-2 text-white md:text-xl xl:text-2xl">2022</p>
+    <p class="pt-2 text-white dark:text-black md:text-xl xl:text-2xl">2022</p>
   </div>
 
   <img src="../assets/img/page2_4.png" class="ml-auto mr-auto mb-3 w-1/3" />
   <div class="flex justify-center">
-    <p class="pr-2 text-white md:text-base xl:text-2xl">JOYCE JONATHAN</p>
-    <p class="pl-2 pb-10 text-white md:text-base xl:text-2xl">14H</p>
+    <p class="pr-2 text-white dark:text-black md:text-base xl:text-2xl">JOYCE JONATHAN</p>
+    <p class="pl-2 pb-10 text-white dark:text-black md:text-base xl:text-2xl">14H</p>
   </div>
 
   <img src="../assets/img/page2_5.png" class="ml-auto mr-auto mb-3 w-1/3" />
   <div class="flex justify-center">
-    <p class="pr-2 text-white md:text-base xl:text-2xl">JESERS</p>
-    <p class="pl-2 pb-10 text-white md:text-base xl:text-2xl">16H</p>
+    <p class="pr-2 text-white dark:text-black md:text-base xl:text-2xl">JESERS</p>
+    <p class="pl-2 pb-10 text-white dark:text-black md:text-base xl:text-2xl">16H</p>
   </div>
 
   <img src="../assets/img/page2_6.png" class="ml-auto mr-auto mb-3 w-1/3" />
   <div class="flex justify-center">
-    <p class="pr-2 text-white md:text-base xl:text-2xl">LA BESTIOLE</p>
-    <p class="pl-2 pb-10 text-white md:text-base xl:text-2xl">18H</p>
+    <p class="pr-2 text-white dark:text-black md:text-base xl:text-2xl">LA BESTIOLE</p>
+    <p class="pl-2 pb-10 text-white dark:text-black md:text-base xl:text-2xl">18H</p>
   </div>
 
   <img src="../assets/img/page2_7.png" class="ml-auto mr-auto mb-3 w-1/3" />
   <div class="flex justify-center">
-    <p class="pr-2 text-white md:text-base xl:text-2xl">L.E.J</p>
-    <p class="pl-2 pb-10 text-white md:text-base xl:text-2xl">20H</p>
+    <p class="pr-2 text-white dark:text-black md:text-base xl:text-2xl">L.E.J</p>
+    <p class="pl-2 pb-10 text-white dark:text-black md:text-base xl:text-2xl">20H</p>
   </div>
 </template>
