f6c25d218656934118740899022d2b9dd68c9acc
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-01 11:19:19 +0200

    dark mode festival

--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -1,31 +1,33 @@
 <template>
-  <div class="bg-black">
-    <p class="text-center font-bold text-white sm:text-lg md:text-xl xl:text-2xl">LE 3,2,1 FESTIVAL</p>
+  <div class="bg-black dark:bg-white">
+    <p class="text-center font-bold text-white dark:text-black sm:text-lg md:text-xl xl:text-2xl">LE 3,2,1 FESTIVAL</p>
     <img src="../assets/img/lignes_page2.svg" class="ml-auto mr-auto mb-7 mt-3 w-3/12" />
 
     <img src="../assets/img/page4_4.png" class="ml-auto mr-auto mb-7 w-2/12 pb-7" />
 
     <div class="mb-7 pb-7 md:px-36 xl:pl-56 xl:pr-64">
-      <p class="text-center font-normal text-white md:text-base xl:text-lg">
+      <p class="text-center font-normal text-white dark:text-black md:text-base xl:text-lg">
         Le 3,2,1 festival est un festival de musique pop/rock. Il se situe à Troyes dans le Grand Est.
       </p>
     </div>
     <img src="../assets/img/page4_5.png" class="ml-auto mr-auto mb-7 w-7/12 pb-7" />
 
     <div class="mb-7 pb-7 md:px-36 xl:pl-56 xl:pr-64">
-      <p class="text-center font-normal text-white md:text-base xl:text-lg">
+      <p class="text-center font-normal text-white dark:text-black md:text-base xl:text-lg">
         Découvrez une panoplie d’artistes connues principalement en France.
       </p>
     </div>
     <img src="../assets/img/page4_6.png" class="ml-auto mr-auto mb-7 w-7/12 pb-7" />
 
     <div class="mb-7 pb-7 md:px-36 xl:pl-56 xl:pr-64">
-      <p class="text-center font-normal text-white md:text-base xl:text-lg">Trois jours de musiques s’offre à vous du 10 au 12 juin.</p>
+      <p class="text-center font-normal text-white dark:text-black md:text-base xl:text-lg">
+        Trois jours de musiques s’offre à vous du 10 au 12 juin.
+      </p>
     </div>
     <img src="../assets/img/page4_7.png" class="ml-auto mr-auto mb-7 w-7/12 pb-7" />
 
     <div class="mb-7 pb-7 md:px-36 xl:pl-56 xl:pr-64">
-      <p class="text-center font-normal text-white md:text-base xl:text-lg">
+      <p class="text-center font-normal text-white dark:text-black md:text-base xl:text-lg">
         Musique, mais pas seulement ! Profitez d’activités extérieures adapté à toute la famille.
       </p>
     </div>
@@ -43,11 +45,13 @@
     </div>
 
     <div class="mb-7 pb-7 md:px-36 xl:pl-56 xl:pr-64">
-      <p class="text-center font-normal text-white md:text-sm xl:text-base">Utilisez la carte pour découvrir le lieu du festival.</p>
+      <p class="text-center font-normal text-white dark:text-black md:text-sm xl:text-base">
+        Utilisez la carte pour découvrir le lieu du festival.
+      </p>
     </div>
 
     <RouterLink to="/programmation">
-      <p class="pt-7 pb-7 text-center text-lg text-white">Découvrez la programmation du festival ici</p>
+      <p class="pt-7 pb-7 text-center text-lg text-white dark:text-black">Découvrez la programmation du festival ici</p>
     </RouterLink>
   </div>
 </template>
