e2cd395e2e5e85286b1cac3d2a9ccb39b0000206
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-01 11:28:46 +0200

    page404 dark mode

--- src/views/P404View.vue
+++ src/views/P404View.vue
@@ -1,8 +1,8 @@
 <template>
   <p class="pt-10 text-center text-7xl text-red-600">Oups</p>
-  <p class="pt-5 text-center text-3xl">Cette page n'existe pas !</p>
+  <p class="pt-5 text-center text-3xl text-black dark:text-white">Cette page n'existe pas !</p>
   <img src="../assets/img/404.webp" class="ml-auto mr-auto mb-1 w-3/12 pb-7" />
   <RouterLink to="/">
-    <p class="mt-5 mb-5 text-center">Revenir à la page d'accueil</p>
+    <p class="mt-5 mb-5 text-center text-black dark:text-white">Revenir à la page d'accueil</p>
   </RouterLink>
 </template>
