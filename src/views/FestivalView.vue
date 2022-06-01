6916e870a6f0a652f297a5707331bd0e2bee50ef
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-01 10:50:56 +0200

    changement carte

--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -30,7 +30,15 @@
       </p>
     </div>
 
-    <img src="../assets/img/carte.png" class="ml-auto mr-auto mb-1 w-2/12 pb-7" />
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d42472.65534387422!2d4.041114905982704!3d48.29244649083673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ee9857e787b7b1%3A0x57dd125566e84f75!2s10000%20Troyes!5e0!3m2!1sfr!2sfr!4v1654073378659!5m2!1sfr!2sfr"
+      width="600"
+      height="450"
+      style="border: 0"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
     <div class="mb-7 pb-7 md:px-36 xl:pl-56 xl:pr-64">
       <p class="text-center font-normal text-white md:text-sm xl:text-base">Utilisez la carte pour découvrir le lieu du festival.</p>
     </div>
