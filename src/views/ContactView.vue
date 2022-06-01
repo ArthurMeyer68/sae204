56dd238d535f82d4c36505ff8b282f9f97ccd23c
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-01 11:25:18 +0200

    dark mode contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,8 +1,8 @@
 <template>
-  <p class="pt-5 text-center font-bold text-black sm:text-lg md:text-xl xl:text-2xl">NOUS CONTACTER</p>
+  <p class="pt-5 text-center font-bold text-black dark:text-white sm:text-lg md:text-xl xl:text-2xl">NOUS CONTACTER</p>
   <img src="../assets/img/lignes2_page2.svg" class="ml-auto mr-auto mb-7 mt-3 w-3/12" />
   <div><Formulaire /></div>
-  <p class="pt-5 pb-7 text-center font-bold text-black sm:text-lg md:text-xl xl:text-xl">321festival@gmail.com</p>
+  <p class="pt-5 pb-7 text-center font-bold text-black dark:text-white sm:text-lg md:text-xl xl:text-xl">321festival@gmail.com</p>
 </template>
 
 <script>
