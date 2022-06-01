9c4fb44c2c145493fd54927d9d47ecffced546f4
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-01 12:49:12 +0200

    dark mode Composant5

--- src/components/Comp5View.vue
+++ src/components/Comp5View.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div class="bg-[url('src/assets/img/home_page_img_13.png')] bg-cover">
-      <div class="py-48 text-center text-gray-50 md:text-base xl:text-2xl">
+      <div class="py-48 text-center text-gray-50 dark:text-gray-900 md:text-base xl:text-2xl">
         <p>FAMILLE</p>
       </div>
     </div>
     <div class="bg-[url('src/assets/img/home_page_img_14.png')] bg-cover">
-      <div class="py-8 text-center text-gray-50 md:text-base xl:text-2xl">
+      <div class="py-8 text-center text-gray-50 dark:text-gray-900 md:text-base xl:text-2xl">
         <p>Personne n’est laissé de côté ! Le 3,2,1 festival est adapté à tous types d’âge.</p>
       </div>
     </div>
