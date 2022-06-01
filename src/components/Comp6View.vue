af2c36a761988d5fb5ce6ecbe7250e70253d7be6
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-01 12:51:40 +0200

    dark mode Composant6

--- src/components/Comp6View.vue
+++ src/components/Comp6View.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="pl-20">
-    <p class="text-white md:text-xl xl:text-2xl">{{ jour }}</p>
+    <p class="text-white dark:text-black md:text-xl xl:text-2xl">{{ jour }}</p>
     <div class="flex">
       <p class="text-red-600 md:text-3xl xl:text-5xl">{{ date }}</p>
-      <p class="pl-3 text-white md:text-3xl xl:text-5xl">{{ mois }}</p>
+      <p class="pl-3 text-white dark:text-black md:text-3xl xl:text-5xl">{{ mois }}</p>
     </div>
 
-    <p class="pt-2 text-white md:text-xl xl:text-2xl">2022</p>
+    <p class="pt-2 text-white dark:text-black md:text-xl xl:text-2xl">2022</p>
   </div>
 
   <img :src="image" alt="" class="ml-auto mr-auto mb-3 w-1/3" />
   <div class="flex justify-center">
-    <p class="pr-2 text-white md:text-base xl:text-2xl">{{ nom }}</p>
-    <p class="pb-10 pl-2 text-white md:text-base xl:text-2xl">{{ heure }}</p>
+    <p class="pr-2 text-white dark:text-black md:text-base xl:text-2xl">{{ nom }}</p>
+    <p class="pb-10 pl-2 text-white dark:text-black md:text-base xl:text-2xl">{{ heure }}</p>
   </div>
 
   <img :src="image2" alt="" class="ml-auto mr-auto mb-3 w-1/3" />
   <div class="flex justify-center">
-    <p class="pr-2 text-white md:text-base xl:text-2xl">{{ nom2 }}</p>
-    <p class="pb-10 pl-2 text-white md:text-base xl:text-2xl">{{ heure2 }}</p>
+    <p class="pr-2 text-white dark:text-black md:text-base xl:text-2xl">{{ nom2 }}</p>
+    <p class="pb-10 pl-2 text-white dark:text-black md:text-base xl:text-2xl">{{ heure2 }}</p>
   </div>
 
   <img :src="image3" alt="" class="ml-auto mr-auto mb-3 w-1/3" />
   <div class="flex justify-center">
-    <p class="pr-2 text-white md:text-base xl:text-2xl">{{ nom3 }}</p>
-    <p class="pl-2 pb-10 text-white md:text-base xl:text-2xl">{{ heure3 }}</p>
+    <p class="pr-2 text-white dark:text-black md:text-base xl:text-2xl">{{ nom3 }}</p>
+    <p class="pl-2 pb-10 text-white dark:text-black md:text-base xl:text-2xl">{{ heure3 }}</p>
   </div>
 </template>
 
