27c8c33aff6377817fe20b4174b6da9e194f6bd7
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-01 10:18:07 +0200

    script guide de style

--- src/views/GuidedestyleView.vue
+++ src/views/GuidedestyleView.vue
@@ -1,9 +1,18 @@
 <template></template>
 <script>
+import Comp1 from "../components/Comp1View.vue";
+import Comp2 from "../components/Comp2View.vue";
+import Comp3 from "../components/Comp3View.vue";
+import Comp4 from "../components/Comp4View.vue";
+import Comp5 from "../components/Comp5View.vue";
 import Comp6 from "../components/Comp6View.vue";
 import Comp7 from "../components/Comp7View.vue";
+import Comp9 from "../components/Comp9View.vue";
+import Comp10 from "../components/Comp10View.vue";
+import Comp11 from "../components/Comp11View.vue";
+import Formulaire from "../components/FormulaireView.vue";
 
 export default {
-  components: { Comp6, Comp7 },
+  components: { Comp1, Comp2, Comp3, Comp4, Comp5, Comp6, Comp7, Comp9, Comp10, Comp11, Formulaire },
 };
 </script>
