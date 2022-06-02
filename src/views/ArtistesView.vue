e7e7d308916ce8f2d02b05e29ca5039d3e2212b8
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-02 08:49:28 +0200

    firebase authentification

--- src/views/ArtistesView.vue
+++ src/views/ArtistesView.vue
@@ -1,38 +1,28 @@
 <template>
-  <p class="pt-7 text-center font-bold text-black dark:text-white sm:text-lg md:text-xl xl:text-2xl">LES ARTISTES</p>
-  <img src="../assets/img/lignes2_page2.svg" class="ml-auto mr-auto mb-7 mt-3 w-3/12 pb-7" />
-
-  <div class="mb-7 md:px-36 xl:pl-56 xl:pr-64">
-    <p class="text-dark mb-7 text-left font-normal dark:text-white md:text-base xl:text-lg">
-      De nombreux artistes pop/rock seront présents au 3,2,1 festival.
-    </p>
-    <p class="text-dark text-left font-normal dark:text-white md:text-base xl:text-lg">
-      Venez découvrir ces artistes qui vous feront voyager pendant leurs prestations.
-    </p>
-  </div>
-
-  <p class="mb-7 text-center font-bold text-black dark:text-white sm:text-lg md:text-lg xl:text-xl">Les artistes présents au festival :</p>
-
   <div>
-    <Comp9 image="/public/page3_1.png" nom="MUSTANG" />
-    <Comp10 image="/public/page3_2.png" nom="JAIN" />
-    <Comp9 image="/public//page3_3.png" nom="FEU! CHATTERTON" />
-    <Comp10 image="/public/page3_4.png" nom="JOYCE JONATHAN" />
-    <Comp9 image="/public//page3_5.png" nom="JESERS" />
-    <Comp10 image="/public/page3_6.png" nom="LA BESTIOLE" />
-    <Comp9 image="/public//page3_7.png" nom="L.E.J" />
-    <Comp10 image="/public/page3_8.png" nom="CANNIBALE" />
-    <Comp9 image="/public//page3_9.png" nom="ANNABELLA HAWK" />
-    <Comp10 image="/public/page3_10.png" nom="LA FEMME" />
+    <h5>Firebase - Se connecter</h5>
+    <form @submit.prevent="onCnx()">
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <button class="btn btn-dark">Email :</button>
+        </div>
+        <input class="form-control" type="text" v-model="user.email" required />
+      </div>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <button class="btn btn-dark">Mot de passe :</button>
+        </div>
+        <input class="form-control" type="password" v-model="user.password" required />
+      </div>
+      <div class="alert alert-warning mb-3 text-center" v-if="message != null">
+        {{ message }}
+      </div>
+      <div>
+        <button class="float-left" @click="onDcnx()">Deconnexion</button>
+        <button variant="dark" class="float-right" type="submit">Connexion</button>
+      </div>
+    </form>
   </div>
-
-  <RouterLink to="/artistesdet">
-    <p class="pt-7 pb-7 text-center text-lg text-black dark:text-white">Découvrez les artistes plus en détail ici</p>
-  </RouterLink>
-
-  <RouterLink to="/programmation">
-    <p class="pt-7 pb-7 text-center text-lg text-black dark:text-white">Découvrez la programmation de ces artistes ici</p>
-  </RouterLink>
 </template>
 
 <script>
@@ -43,10 +33,50 @@ export default {
   components: { Comp9, Comp10 },
 
   data() {
-    return {};
+    return {
+      user: {
+        email: null,
+        password: null,
+      },
+      message: null,
+    };
   },
 
   mounted() {},
+
+  methods: {
+    onCnx() {
+      // Se connecter avec user et mot de passe
+      signInWithEmailAndPassword(getAuth(), this.user.email, this.user.password)
+        .then((response) => {
+          // Connexion OK
+          console.log("user connecté", response.user);
+          this.user = response.user;
+          this.message = "User connecté : " + this.user.email;
+        })
+        .catch((error) => {
+          // Erreur de connexion
+          console.log("Erreur connexion", error);
+          this.message = "Erreur d'authentification";
+        });
+    },
+    onDcnx() {
+      // Se déconnecter
+      signOut(getAuth())
+        .then((response) => {
+          this.user = getAuth().currentUser;
+          this.user = {
+            email: null,
+            password: null,
+          };
+          console.log("user deconnecté ", this.user);
+          this.message = "user non connecté";
+        })
+        .catch((error) => {
+          console.log("erreur deconnexion ", error);
+        });
+    },
+  },
 };
 </script>
 
