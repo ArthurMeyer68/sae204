0a3675510652dba92a23d971b6e34e0072e7d938
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-03 13:23:02 +0200

    guide de style finis

--- src/views/GuidedestyleView.vue
+++ src/views/GuidedestyleView.vue
@@ -1,16 +1,32 @@
 <template>
   <p class="mt-10 text-center text-2xl text-black">Guide de style</p>
-  <div class="pt-10"><Comp1 /></div>
-  <div class="pt-10"><Comp2 /></div>
-  <div class="pt-10"><Comp3 /></div>
-  <div class="pt-10"><Comp4 /></div>
-  <div class="pt-10"><Comp5 /></div>
-  <div class="pt-10"><Comp6 /></div>
-  <div class="bg-black pt-10"><Comp7 /></div>
-  <div class="pt-10"><Comp9 /></div>
-  <div class="pt-10"><Comp10 /></div>
-  <div class="pt-10"><Comp11 /></div>
-  <div class="pt-10"><Formulaire /></div>
+
+  <div class="flex h-32 w-1/3 p-2">
+    <p class="text-2xl">Couleurs</p>
+    <div class="h-32 w-32 rounded-full bg-red-600 pt-3 pb-5"></div>
+    <div class="h-32 w-32 rounded-full border-4 bg-white pt-3 pb-5"></div>
+    <div class="h-32 w-32 rounded-full bg-gray-900 pt-3 pb-5"></div>
+    <div class="h-32 w-32 rounded-full bg-gray-800 pt-3 pb-5"></div>
+  </div>
+
+  <p class="pb-3 text-2xl">Fonts</p>
+  <p class="text-base">Yu Gothic UI - text-base</p>
+  <p class="text-lg">Yu Gothic UI - text-lg</p>
+  <p class="text-xl">Yu Gothic UI - text-xl</p>
+  <p class="text-2xl">Yu Gothic UI - text-2xl</p>
+  <p class="text-xl font-bold">Yu Gothic UI - text-xl</p>
+
+  <div class="ml-auto mr-auto w-1/3 pt-10"><Comp1 /></div>
+  <div class="ml-auto mr-auto w-1/3 pt-10"><Comp2 /></div>
+  <div class="ml-auto mr-auto w-1/3 pt-10"><Comp3 /></div>
+  <div class="ml-auto mr-auto w-1/3 pt-10"><Comp4 /></div>
+  <div class="ml-auto mr-auto w-1/3 pt-10"><Comp5 /></div>
+  <div class="ml-auto mr-auto w-1/3 pt-10"><Comp6 /></div>
+  <div class="ml-auto mr-auto w-1/3 bg-black pt-10"><Comp7 /></div>
+  <div class="ml-auto mr-auto w-1/3 pt-10"><Comp9 /></div>
+  <div class="ml-auto mr-auto w-1/3 pt-10"><Comp10 /></div>
+  <div class="ml-auto mr-auto w-1/3 pt-10"><Comp11 /></div>
+  <div class="ml-auto mr-auto w-1/3 pt-10"><Formulaire /></div>
 </template>
 <script>
 import Comp1 from "../components/Comp1View.vue";
