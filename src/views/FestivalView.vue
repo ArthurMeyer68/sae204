eba52d66823772717a0608629db68f08e9362efd
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-03 15:54:51 +0200

    festival changement img public

--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="bg-black dark:bg-white">
     <p class="text-center font-bold text-white dark:text-black sm:text-lg md:text-xl xl:text-2xl">LE 3,2,1 FESTIVAL</p>
-    <img src="../assets/img/lignes_page2.svg" class="ml-auto mr-auto mb-7 mt-3 w-3/12" />
+    <img src="/lignes_page2.svg" class="ml-auto mr-auto mb-7 mt-3 w-3/12" />
 
-    <img src="../assets/img/page4_4.png" class="ml-auto mr-auto mb-7 w-2/12 pb-7" />
+    <img src="/page4_4.png" class="ml-auto mr-auto mb-7 w-2/12 pb-7" />
 
     <div class="mb-7 pb-7 md:px-36 xl:pl-56 xl:pr-64">
       <p class="text-center font-normal text-white dark:text-black md:text-base xl:text-lg">
         Le 3,2,1 festival est un festival de musique pop/rock. Il se situe à Troyes dans le Grand Est.
       </p>
     </div>
-    <img src="../assets/img/page4_5.png" class="ml-auto mr-auto mb-7 w-7/12 pb-7" />
+    <img src="/page4_5.png" class="ml-auto mr-auto mb-7 w-7/12 pb-7" />
 
     <div class="mb-7 pb-7 md:px-36 xl:pl-56 xl:pr-64">
       <p class="text-center font-normal text-white dark:text-black md:text-base xl:text-lg">
         Découvrez une panoplie d’artistes connues principalement en France.
       </p>
     </div>
-    <img src="../assets/img/page4_6.png" class="ml-auto mr-auto mb-7 w-7/12 pb-7" />
+    <img src="/page4_6.png" class="ml-auto mr-auto mb-7 w-7/12 pb-7" />
 
     <div class="mb-7 pb-7 md:px-36 xl:pl-56 xl:pr-64">
       <p class="text-center font-normal text-white dark:text-black md:text-base xl:text-lg">
         Trois jours de musiques s’offre à vous du 10 au 12 juin.
       </p>
     </div>
-    <img src="../assets/img/page4_7.png" class="ml-auto mr-auto mb-7 w-7/12 pb-7" />
+    <img src="/page4_7.png" class="ml-auto mr-auto mb-7 w-7/12 pb-7" />
 
     <div class="mb-7 pb-7 md:px-36 xl:pl-56 xl:pr-64">
       <p class="text-center font-normal text-white dark:text-black md:text-base xl:text-lg">
