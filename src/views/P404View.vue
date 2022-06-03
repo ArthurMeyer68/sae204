69919647058d2853448b130e19b9ee6b2ba84d7a
ArthurMeyer68 <95077013+ArthurMeyer68@users.noreply.github.com>
2022-06-03 15:58:16 +0200

    img en public fait

--- src/views/P404View.vue
+++ src/views/P404View.vue
@@ -1,7 +1,7 @@
 <template>
   <p class="pt-10 text-center text-7xl text-red-600">Oups</p>
   <p class="pt-5 text-center text-3xl text-black dark:text-white">Cette page n'existe pas !</p>
-  <img src="../assets/img/404.webp" class="ml-auto mr-auto mb-1 w-3/12 pb-7" />
+  <img src="/404.webp" class="ml-auto mr-auto mb-1 w-3/12 pb-7" />
   <RouterLink to="/">
     <p class="mt-5 mb-5 text-center text-black dark:text-white">Revenir à la page d'accueil</p>
   </RouterLink>
